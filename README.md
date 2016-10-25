721e6b49bfc3f3c802037ed6d83a089894079609
Nicolas Legrand <Nicolas.Legrand@realdolmen.com>
2016-10-25 09:49:32 +0200

    added projects

---
+++ README.md
@@ -0,0 +1,1 @@
+# Realdolmen_Devops
---
+++ TeamCityTest/TeamCityTestProjectUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TeamCityTestProjectUnitTests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+
+            Assert.IsTrue(false);
+        }
+    }
+}
