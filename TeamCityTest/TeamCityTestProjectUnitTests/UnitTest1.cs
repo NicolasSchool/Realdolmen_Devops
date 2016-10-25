3885aadf77d4c86712e977f9b6dd180fa4a28ac7
Nicolas Legrand <Nicolas.Legrand@realdolmen.com>
2016-10-25 10:34:41 +0200

    test pass

--- TeamCityTest/TeamCityTestProjectUnitTests/UnitTest1.cs
+++ TeamCityTest/TeamCityTestProjectUnitTests/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace TeamCityTestProjectUnitTests {
         [TestMethod]
         public void TestMethod1() {
 
-            Assert.IsTrue(false);
+            Assert.IsTrue(true);
         }
     }
 }
